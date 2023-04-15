dd6635ed491515a017681fe9dc6c88b8e34fd163
stone <564609719@qq.com>
2023-04-15 23:42:32 +0800

    给fortran库生成的头文件添加#pragma onece，并且优化了cmakelist

--- main/inc/c_interface.h
+++ main/inc/c_interface.h
@@ -1,3 +1,4 @@
+#pragma once
 int add_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ (int *a /* WARNING: non-interoperable KIND */ , int *b /* WARNING: non-interoperable KIND */ );
 int foo_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ ();
 int helloworld_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ ();
